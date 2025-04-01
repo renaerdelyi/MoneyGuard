ad865c4cf267f625adcca26c24721d4c76ec97ac
renaerdelyi <rena.szabo@yahoo.com>
2025-04-01 20:05:23 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import Sidebar from './Sidebar/Sidebar';
-import Header from './Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as  Routes, Route, Navigate } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "././redux/store";
+import PrivateRoute from "./redux/routes/PrivateRoute";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import DashboardPage from "./pages/DashboardPage";
 
 export const App = () => {
+
+  
   return (
-    <div>
-      <Header />
-      <div style={{ display: 'flex', height: 'calc(100vh - 60px)' }}>
-        <Sidebar />
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <Sidebar /> {/* Sidebar rămâne pe toate paginile */}
+
         <div style={{ flex: 1, background: '#f5f5f5' }}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/dashboard" element={<DashboardPage />} />
+            </Route>
+          </Routes>
         </div>
       </div>
-    </div>
+    </PersistGate>
+  </Provider>
   );
 };
+
+
